b46e5ca10463b02aaf6800d4ece3e28c9bbe2cd2
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2025-04-23 10:46:22 +0200

    DB Context

---
+++ IotProject.Shared/Models/Database/Base.cs
@@ -0,0 +1,9 @@
+﻿namespace IotProject.Shared.Models.Database
+{
+    public class Base
+    {
+        public string Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
---
+++ IotProject.Shared/Models/Database/Device.cs
@@ -0,0 +1,15 @@
+﻿namespace IotProject.Shared.Models.Database
+{
+    public class Device : Base
+    {
+        public string OwnerId { get; set; }
+        public User Owner { get; set; }
+        public string? RoomId { get; set; }
+        public Room? Room { get; set; }
+        public string DeviceType { get; set; }
+        public ICollection<DeviceData> Data { get; set; } = new List<DeviceData>();
+        public string ApiKey { get; set; }
+        public string Config { get; set; }
+        
+    }
+}
