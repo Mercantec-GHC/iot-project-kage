a06baacdc58a2644c5c75ff1061184830b5bfed0
Tobias <115145472+TobiasDK22@users.noreply.github.com>
2025-04-24 10:30:21 +0200

    Registration & Login endpoints.

---
+++ IotProject.Shared/Models/Requests/AuthRequests.cs
@@ -0,0 +1,17 @@
+﻿namespace IotProject.Shared.Models.Requests
+{
+    public class UserCreateRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class UserLoginRequest
+    {
+        public string Email { get; set;}
+        public string Password { get; set; }
+    }
+}
---
+++ IotProject.Shared/Models/Responses/UserResponses.cs
@@ -0,0 +1,5 @@
+﻿namespace IotProject.Shared.Models.Responses
+{
+    public record UserCreateResponse(string Id, string Message = null!);
+    public record UserLoginResponse(string Token,int Expires, string RefreshToken, string Message = null!);
+}
