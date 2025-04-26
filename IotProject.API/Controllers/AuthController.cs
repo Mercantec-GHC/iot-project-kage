158824b299ddb615e880be36519099b068d9213a
Nikolaj Have <nhave06@gmail.com>
2025-04-26 18:14:21 +0200

    Changed the SecuritySchemeType in Swagger from ApiKey to Http.

--- IotProject.API/Controllers/AuthController.cs
+++ IotProject.API/Controllers/AuthController.cs
@@ -14,7 +14,7 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace IotProject.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class AuthController(AppDbContext context, IConfiguration configuration) : ControllerBase
     {
