1e49fefcb71289bd4666e82e2ef0e4443fa9402e
Nikolaj Have <nhave06@gmail.com>
2025-04-26 19:07:45 +0200

    Created an MD file for diagrams. Prepared a class library for authenticantion.

---
+++ IotProject.Auth/Extensions/AuthExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class AuthExtension
+    {
+        public static IServiceCollection AddJwtAuth(this IServiceCollection services)
+        {
+            // Add stuff here...
+
+            return services;
+        }
+    }
+}
