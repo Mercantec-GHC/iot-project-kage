868856010c4f8204b4f0636ceea205b3444dfcff
Nikolaj Have <nhave06@gmail.com>
2025-04-26 19:13:25 +0200

    Created a project for the Maui mobile application.

---
+++ IotProject.Mobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace IotProject.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
