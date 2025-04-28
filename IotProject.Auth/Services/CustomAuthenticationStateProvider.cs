58530105925acd19789cf89916758e545a5d47da
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2025-04-28 13:30:59 +0200

    Blazor authentication and login page.

---
+++ IotProject.Auth/Services/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,50 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace IotProject.Auth.Services
+{
+    public class CustomAuthenticationStateProvider(HttpClient httpClient, ILocalStorageService localStorage) : AuthenticationStateProvider
+    {
+        private static AuthenticationState Anonymous => new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                var savedToken = await localStorage.GetItemAsync<string>("JwtToken");
+                if (string.IsNullOrWhiteSpace(savedToken))
+                {
+                    return Anonymous;
+                }
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(savedToken), "jwt")));
+            }
+            catch
+            {
+                return Anonymous;
+            }
+        }
+
+        public void MarkUserAsAuthenticated(string token)
+        {
+            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt"));
+            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public void MarkUserAsLoggedOut()
+        {
+            var anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
+            var authState = Task.FromResult(new AuthenticationState(anonymousUser));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(jwt) as JwtSecurityToken;
+            return jsonToken?.Claims!;
+        }
+    }
+}
