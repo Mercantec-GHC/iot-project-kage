7e96298ff1b83e8f8ad8c0521bb91cde8e0dfe0a
Nikolaj Have <nhave06@gmail.com>
2025-04-29 14:44:55 +0200

    Comments + Docker restart: unless-stopped

--- IotProject.Auth/Services/CustomAuthenticationStateProvider.cs
+++ IotProject.Auth/Services/CustomAuthenticationStateProvider.cs
@@ -8,21 +8,32 @@ namespace IotProject.Auth.Services
 {
     public class CustomAuthenticationStateProvider(ILocalStorageService localStorage, ISessionStorageService sessionStorage) : AuthenticationStateProvider
     {
+        // A predefined Anonymous Authentication State.
         private static AuthenticationState Anonymous => new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
 
+        /// <summary>
+        /// Retrieves the Authentication State of the current user.
+        /// </summary>
+        /// <returns>The state of the user if they are signed in, otherwise returns an anonymous state.</returns>
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
+
+            // Using try / catch to handle server side rendering.
             try
             {
+                // Tries to obtain a token from client local storage.
                 var savedToken = await localStorage.GetItemAsync<string>("JwtToken");
                 if (string.IsNullOrWhiteSpace(savedToken))
                 {
+                    // Tries to obtain a token from client session storage.
                     savedToken = await sessionStorage.GetItemAsync<string>("JwtToken");
                     if (string.IsNullOrWhiteSpace(savedToken))
                     {
+                        // If no token exsists, return the Anonymous state.
                         return Anonymous;
                     }
                 }
+                // Create an Authentication State from the saved token.
                 return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(savedToken), "jwt")));
             }
             catch
@@ -31,6 +42,10 @@ namespace IotProject.Auth.Services
             }
         }
 
+        /// <summary>
+        /// Marks the user as signed in.
+        /// </summary>
+        /// <param name="token">JWT token containing the users claims.</param>
         public void MarkUserAsAuthenticated(string token)
         {
             var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt"));
@@ -38,6 +53,9 @@ namespace IotProject.Auth.Services
             NotifyAuthenticationStateChanged(authState);
         }
 
+        /// <summary>
+        /// Marks the user as signed out.
+        /// </summary>
         public void MarkUserAsLoggedOut()
         {
             var anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
@@ -45,6 +63,11 @@ namespace IotProject.Auth.Services
             NotifyAuthenticationStateChanged(authState);
         }
 
+        /// <summary>
+        /// Parses the users claims from a JWT token.
+        /// </summary>
+        /// <param name="jwt"></param>
+        /// <returns>A list of claims.</returns>
         public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
         {
             var handler = new JwtSecurityTokenHandler();
