1adc442c0b85eee7d8f16d01ceb6c46e5ea793cb
Nikolaj Have <nhave06@gmail.com>
2025-04-29 13:13:39 +0200

    Refresh token client side.

--- IotProject.Auth/Services/AuthService.cs
+++ IotProject.Auth/Services/AuthService.cs
@@ -5,6 +5,7 @@ using IotProject.Shared.Models.Requests;
 using System.Text.Json;
 using System.Text;
 using IotProject.Shared.Models.Responses;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace IotProject.Auth.Services
 {
@@ -12,7 +13,7 @@ namespace IotProject.Auth.Services
     {
         private static JsonSerializerOptions JsonOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
-        public async Task<bool> Login(UserLoginRequest request)
+        public async Task<bool> Login(UserLoginRequest request, bool remember = false)
         {
             var loginAsJson = JsonSerializer.Serialize(request);
             var response = await httpClient.PostAsync("auth/login", new StringContent(loginAsJson, Encoding.UTF8, "application/json"));
@@ -22,17 +23,67 @@ namespace IotProject.Auth.Services
             }
 
             var loginResult = JsonSerializer.Deserialize<UserLoginResponse>(await response.Content.ReadAsStringAsync(), JsonOptions);
-            await localStorage.SetItemAsync("JwtToken", loginResult!.Token);
-            await localStorage.SetItemAsync("RefreshToken", loginResult.RefreshToken);
+            if (remember)
+            {
+                await localStorage.SetItemAsync("JwtToken", loginResult!.Token);
+                await localStorage.SetItemAsync("RefreshToken", loginResult.RefreshToken);
+            }
+            else
+            {
+                await sessionStorage.SetItemAsync("JwtToken", loginResult!.Token);
+                await sessionStorage.SetItemAsync("RefreshToken", loginResult.RefreshToken);
+            }
             ((CustomAuthenticationStateProvider)authenticationStateProvider).MarkUserAsAuthenticated(loginResult.Token);
             
             return true;
-        } 
+        }
+
+        public async Task<bool> Refresh()
+        {
+            var refreshToken = await localStorage.GetItemAsync<string>("RefreshToken");
+            bool remember = true;
+
+            if (string.IsNullOrWhiteSpace(refreshToken))
+            {
+                refreshToken = await sessionStorage.GetItemAsync<string>("RefreshToken");
+                remember = false;
+                if (string.IsNullOrWhiteSpace(refreshToken)) return false;
+            }
+
+            UserRefreshRequest request = new UserRefreshRequest
+            {
+                Token = refreshToken,
+            };
+            var refreshAsJson = JsonSerializer.Serialize(request);
+            var response = await httpClient.PostAsync("auth/refresh", new StringContent(refreshAsJson, Encoding.UTF8, "application/json"));
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return false;
+            }
+
+            var loginResult = JsonSerializer.Deserialize<UserLoginResponse>(await response.Content.ReadAsStringAsync(), JsonOptions);
+            if (remember)
+            {
+                await localStorage.SetItemAsync("JwtToken", loginResult!.Token);
+                await localStorage.SetItemAsync("RefreshToken", loginResult.RefreshToken);
+            }
+            else
+            {
+                await sessionStorage.SetItemAsync("JwtToken", loginResult!.Token);
+                await sessionStorage.SetItemAsync("RefreshToken", loginResult.RefreshToken);
+            }
+            ((CustomAuthenticationStateProvider)authenticationStateProvider).MarkUserAsAuthenticated(loginResult.Token);
+
+            return true;
+        }
 
         public async Task Logout()
         {
             await localStorage.RemoveItemAsync("JwtToken");
             await localStorage.RemoveItemAsync("RefreshToken");
+            await sessionStorage.RemoveItemAsync("JwtToken");
+            await sessionStorage.RemoveItemAsync("RefreshToken");
             ((CustomAuthenticationStateProvider)authenticationStateProvider).MarkUserAsLoggedOut();
         }
 
@@ -47,5 +98,45 @@ namespace IotProject.Auth.Services
 
             return JsonSerializer.Deserialize<UserCreateResponse>(await response.Content.ReadAsStringAsync(), JsonOptions);
 		}
+
+        /// <summary>
+        /// To be used on page change. Verifies and requests new tokens, otherwise signs out the user.
+        /// </summary>
+        /// <returns>If the current user is signed in.</returns>
+        public async Task<bool> ConfirmTokens()
+        {
+            // Using try / catch to handle server side rendering.
+            try
+            {
+                var savedToken = await localStorage.GetItemAsync<string>("JwtToken");
+                if (string.IsNullOrWhiteSpace(savedToken))
+                {
+                    savedToken = await sessionStorage.GetItemAsync<string>("JwtToken");
+                    if (string.IsNullOrWhiteSpace(savedToken))
+                    {
+                        await Logout();
+                        return false;
+                    }
+                }
+
+                var handler = new JwtSecurityTokenHandler();
+                var jsonToken = handler.ReadToken(savedToken) as JwtSecurityToken;
+                var expiry = jsonToken?.ValidTo;
+
+                if (expiry.HasValue && expiry.Value < DateTime.UtcNow)
+                {
+                    if (!await Refresh())
+                    {
+                        await Logout();
+                        return false;
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
