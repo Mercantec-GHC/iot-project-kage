68646ae53e6f5eef4ff3ba2a1f89ed62d23cc07e
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2025-05-01 09:09:15 +0200

    Added a delete user end point

--- IotProject.Shared/Models/Requests/UserRequests.cs
+++ IotProject.Shared/Models/Requests/UserRequests.cs
@@ -5,22 +5,22 @@ namespace IotProject.Shared.Models.Requests
     public class UserCreateRequest
     {
 		[Required(ErrorMessage = "First name is required.")]
-		public required string FirstName { get; set; }
+		public string FirstName { get; set; }
 
 		[Required(ErrorMessage = "Last name is required.")]
-		public required string LastName { get; set; }
+		public string LastName { get; set; }
 
         [Required(ErrorMessage = "Email is required.")]
-        public required string Email { get; set; }
+        public string Email { get; set; }
 
 		[Required(ErrorMessage = "Password is required.")]
         [StringLength(64, ErrorMessage = "Password must be at least 8 characters.", MinimumLength = 8)]
         [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_])[^\s]{8,}$", ErrorMessage = "Password is not secure.")]
-        public required string Password { get; set; }
+        public string Password { get; set; }
 
 		[Required(ErrorMessage = "Confirm password is required.")]
         [Compare("Password", ErrorMessage = "Passwords do not match.")]
-		public required string ConfirmPassword { get; set; }
+		public string ConfirmPassword { get; set; }
     }
 
     public class UserLoginRequest
@@ -34,7 +34,7 @@ namespace IotProject.Shared.Models.Requests
 
     public class UserRefreshRequest
     {
-        public required string Token { get; set; }
+        public string Token { get; set; }
     }
 
     public class UserEditInformationRequest
@@ -49,10 +49,16 @@ namespace IotProject.Shared.Models.Requests
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(64, ErrorMessage = "Password must be at least 8 characters.",  MinimumLength = 8)]
         [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_])[^\s]{8,}$", ErrorMessage = "Password is not secure.")]
-        public required string Password { get; set; }
+        public string Password { get; set; }
 
         [Required(ErrorMessage = "Confirm password is required.")]
         [Compare("Password", ErrorMessage = "Passwords do not match.")]
-        public required string ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class UserDeleteRequest
+    {
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
     }
 }
