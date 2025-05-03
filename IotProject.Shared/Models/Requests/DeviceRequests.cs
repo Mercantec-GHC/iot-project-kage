48628b2451e54dbb3d0850931f307e1e10862b5e
Nikolaj Have <nhave06@gmail.com>
2025-05-03 17:39:47 +0200

    Mobile & Device Controller.

---
+++ IotProject.Shared/Models/Requests/DeviceRequests.cs
@@ -0,0 +1,9 @@
+﻿namespace IotProject.Shared.Models.Requests
+{
+    public class DeviceRegisterRequest
+    {
+        public string DeviceType { get; set; }
+        public string OwnerId { get; set; }
+        public string Config { get; set; }
+    }
+}
