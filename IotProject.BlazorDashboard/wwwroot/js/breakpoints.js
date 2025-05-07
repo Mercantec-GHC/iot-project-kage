87677a438860375e2e5280f61dd041ac47c6127f
Tobias <115145472+TobiasDK22@users.noreply.github.com>
2025-05-07 12:52:50 +0200

    DeviceList page and Breakpoints javascript onResize event.

---
+++ IotProject.BlazorDashboard/wwwroot/js/breakpoints.js
@@ -0,0 +1,23 @@
+﻿export function getBootstrapBreakpoint() {
+    const width = window.innerWidth;
+    if (width < 576) {
+        return 'xs';
+    } else if (width < 768) {
+        return 'sm';
+    } else if (width < 992) {
+        return 'md';
+    } else if (width < 1200) {
+        return 'lg';
+    } else if (width < 1400) {
+        return 'xl';
+    } else {
+        return 'xxl';
+    }
+}
+
+export function onResize(dotnetHelper) {
+    window.addEventListener('resize', () => {
+        const breakpoint = getBootstrapBreakpoint();
+        dotnetHelper.invokeMethodAsync('OnResize', breakpoint);
+    });
+}
