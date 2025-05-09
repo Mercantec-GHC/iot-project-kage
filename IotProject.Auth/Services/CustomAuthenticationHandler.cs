77b8cd945f39325df4a0af2841cd9df438856ad2
nhave <nhave06@gmail.com>
2025-05-09 08:10:11 +0200

    Attribute Authorize fixed using dummy handler and created a LoginRedirect.razor to handle unauthorized redirects. (#74)
    * Attribute Authorize fixed using dummy handler and created a LoginRedirect.razor to handle unauthorized redirects.
    
    * app.UseAuthentication();
    app.UseAuthorization();

---
+++ IotProject.Auth/Services/CustomAuthenticationHandler.cs
@@ -0,0 +1,39 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace IotProject.Auth.Services
+{
+    public class CustomAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public CustomAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        { }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Name, "DummyUser"),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(ClaimTypes.Role, "User")
+            };
+
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return Task.FromResult(AuthenticateResult.Success(ticket));
+        }
+
+    }
+}
