714233ed5bad801a34804f701b6f71ec14c7e8b8
nhave <nhave06@gmail.com>
2025-05-13 11:09:55 +0200

    Add DeviceConfig tabel to database and new Migration. (#87) (#88)
    * merge main to stine (#86)
    
    * Started on account page
    
    * Attribute Authorize fixed using dummy handler and created a LoginRedirect.razor to handle unauthorized redirects.
    
    * app.UseAuthentication();
    app.UseAuthorization();
    
    * Attribute Authorize fixed using dummy handler and created a LoginRedirect.razor to handle unauthorized redirects. (#74)
    
    * Attribute Authorize fixed using dummy handler and created a LoginRedirect.razor to handle unauthorized redirects.
    
    * app.UseAuthentication();
    app.UseAuthorization();
    
    * Fix conflicts in auth (#76)
    
    * Attribute Authorize fixed using dummy handler and created a LoginRedirect.razor to handle unauthorized redirects.
    
    * app.UseAuthentication();
    app.UseAuthorization();
    
    * Added user checks to the room controller.
    
    * Added user checks to the room controller. (#77)
    
    * Attribute Authorize fixed using dummy handler and created a LoginRedirect.razor to handle unauthorized redirects.
    
    * app.UseAuthentication();
    app.UseAuthorization();
    
    * Added user checks to the room controller.
    
    * The user can now change their FirstName, LastName and Email on the account page (#82)
    
    * Device naming, room & removal endpoints.
    * Added GetDevices endpoint to the room controller.
    * Changed RoomCreateResponse to include the room id.
    * Created device type utilities.
    
    * Removed WeatherForecastController.cs & WeatherForecast.cs
    
    ---------
    
    
    
    
    
    
    * Add DeviceConfig tabel to database and new Migration.
    
    ---------
    
    Co-authored-by: Tobias <115145472+TobiasDK22@users.noreply.github.com>
    Co-authored-by: StineDk <144903306+StineDk@users.noreply.github.com>
    Co-authored-by: OPGCoding <97985921+OPGCoding@users.noreply.github.com>
    Co-authored-by: BimSe <97985921+B1mSe@users.noreply.github.com>

---
+++ IotProject.Shared/Models/Database/DeviceConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace IotProject.Shared.Models.Database
+{
+	public class DeviceConfig
+	{
+		public int Id { get; set; }
+		public string DeviceId { get; set; }
+		public Device Device { get; set; }
+		public Dictionary<string, object> Config { get; set; }
+		public DateTime Timestamp { get; set; }
+	}
+}
