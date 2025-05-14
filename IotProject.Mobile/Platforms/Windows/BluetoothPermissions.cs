0cf86571d23e5db9e034793cbc92dd1c236aa25e
nhave <nhave06@gmail.com>
2025-05-14 12:15:04 +0200

    Handle bluetooth permissions on iphone and mac. (#94)
    * Add DeviceConfig tabel to database and new Migration. (#87) (#88)
    
    * merge main to stine (#86)
    
    * Started on account page
    
    * Attribute Authorize fixed using dummy handler and created a LoginRedirect.razor to handle unauthorized redirects.
    
    * app.UseAuthentication();
    app.UseAuthorization();
    
    * Attribute Authorize fixed using dummy handler and created a LoginRedirect.razor to handle unauthorized redirects. (#74)
    
    * Attribute Authorize fixed using dummy handler and created a LoginRedirect.razor to handle unauthorized redirects.
    
    * app.UseAuthentication();
    app.UseAuthorization();
    
    * Fix conflicts in auth (#76)
    
    * Attribute Authorize fixed using dummy handler and created a LoginRedirect.razor to handle unauthorized redirects.
    
    * app.UseAuthentication();
    app.UseAuthorization();
    
    * Added user checks to the room controller.
    
    * Added user checks to the room controller. (#77)
    
    * Attribute Authorize fixed using dummy handler and created a LoginRedirect.razor to handle unauthorized redirects.
    
    * app.UseAuthentication();
    app.UseAuthorization();
    
    * Added user checks to the room controller.
    
    * The user can now change their FirstName, LastName and Email on the account page (#82)
    
    * Device naming, room & removal endpoints.
    * Added GetDevices endpoint to the room controller.
    * Changed RoomCreateResponse to include the room id.
    * Created device type utilities.
    
    * Removed WeatherForecastController.cs & WeatherForecast.cs
    
    ---------
    
    
    
    
    
    
    * Add DeviceConfig tabel to database and new Migration.
    
    ---------
    
    Co-authored-by: Tobias <115145472+TobiasDK22@users.noreply.github.com>
    Co-authored-by: StineDk <144903306+StineDk@users.noreply.github.com>
    Co-authored-by: OPGCoding <97985921+OPGCoding@users.noreply.github.com>
    Co-authored-by: BimSe <97985921+B1mSe@users.noreply.github.com>
    
    * Handle bluetooth permissions on iphone and mac.
    
    * Migrations
    
    ---------
    
    Co-authored-by: Tobias <115145472+TobiasDK22@users.noreply.github.com>
    Co-authored-by: StineDk <144903306+StineDk@users.noreply.github.com>
    Co-authored-by: OPGCoding <97985921+OPGCoding@users.noreply.github.com>
    Co-authored-by: BimSe <97985921+B1mSe@users.noreply.github.com>

---
+++ IotProject.Mobile/Platforms/Windows/BluetoothPermissions.cs
@@ -0,0 +1,10 @@
+﻿namespace IotProject.Mobile
+{
+    public static class BluetoothPermisions
+    {
+        public static async Task<bool> CheckAndRequestPermissions()
+        {
+            return true;
+        }
+    }
+}
