4c47e5b5f20c4f0155ce88c1296e17c7582c8ffd
StineDk <144903306+StineDk@users.noreply.github.com>
2025-05-19 08:39:37 +0200

    Change DeviceRegisterRequest class

--- IotProject.Shared/Models/Requests/DeviceRequests.cs
+++ IotProject.Shared/Models/Requests/DeviceRequests.cs
@@ -4,7 +4,6 @@
     {
         public string DeviceType { get; set; }
         public string OwnerId { get; set; }
-        public string Config { get; set; }
     }
     public class DeviceRemoveRequest
     {
