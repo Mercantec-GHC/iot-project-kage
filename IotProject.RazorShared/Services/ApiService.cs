24f2bf94666358e7a5e676552502319a5f456361
Nikolaj Have <nhave06@gmail.com>
2025-05-19 13:15:56 +0200

    Bluetooth Device Setup now works!!! :D

--- IotProject.RazorShared/Services/ApiService.cs
+++ IotProject.RazorShared/Services/ApiService.cs
@@ -4,6 +4,9 @@ using System.Net.Http;
 using Blazored.LocalStorage;
 using Blazored.SessionStorage;
 using System.Text.Json;
+using IotProject.Shared.Models.Requests;
+using System.Text;
+using IotProject.Shared.Models.Responses;
 
 namespace IotProject.RazorShared.Services
 {
@@ -11,6 +14,21 @@ namespace IotProject.RazorShared.Services
     {
         // Predefines the JsonOptions, to be used by various functions.
         private readonly JsonSerializerOptions JsonOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+        public async Task<string?> RegisterDevice(DeviceRegisterRequest request)
+        {
+            if (!await authorize()) return null;
+
+            var requestBody = JsonSerializer.Serialize(request);
+            var response = await httpClient.PostAsync("device/register", new StringContent(requestBody, Encoding.UTF8, "application/json"));
+
+            if (!response.IsSuccessStatusCode) return null;
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            return result;
+        }
+
 
         private async Task<bool> authorize()
         {
