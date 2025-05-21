43126eb80283b631914c13b0fdb13930c72b3e58
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2025-05-21 11:05:36 +0200

    Room Images

---
+++ IotProject.Shared/Models/Database/RoomImage.cs
@@ -0,0 +1,11 @@
+﻿namespace IotProject.Shared.Models.Database
+{
+    public class RoomImage
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+        public string FileName { get; set; }
+        public string Image { get; set; }
+    }
+}
